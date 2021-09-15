fdaa288bda84fed4f6fbbea9c6b2b8acc938c549
Renato S <brickworked@gmail.com>
2021-09-14 22:20:21 -0400

    Add fade in for splash and home page
    Added rendering of the splash page and then the homepage afterwards

--- nasa-explore/src/App.tsx
+++ nasa-explore/src/App.tsx
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import Splash from './components/splash/Splash';
+import Home from './components/home/Home';
+import FadeIn from 'react-fade-in';
 
-function App() {
+export default function App() {
+  const [count, setCount] = useState(1);
+  const [splashVisibility, setSplash] = useState(true);
+  const [splashFadedOut, setSplashFaded] = useState(false);
+  const [homeVisibility, setHome] = useState(false);
+
+  function fadeSplash(){
+    setCount(count + 1);
+    if (count === 2){
+      // Splash finished fading out, de-render it and render home
+      setSplashFaded(true);
+      setHome(true);
+    }
+    else{
+      // Fade in finished, start fade out
+      setSplash(false);
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      { splashFadedOut === false && 
+        <FadeIn transitionDuration={4000} onComplete={fadeSplash} visible={splashVisibility}>
+          <Splash/>
+        </FadeIn>
+      }
+      { homeVisibility === true &&
+      <FadeIn transitionDuration={2000}>
+        <Home/>
+      </FadeIn>
+      }
     </div>
   );
 }
-
-export default App;
