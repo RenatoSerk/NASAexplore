3d34303c32c65d93f1b32489806f68c2366ffc2d
Renato S <brickworked@gmail.com>
2021-09-15 22:28:04 -0400

    Add header to homepage
    Added a header with logo to the home component

--- nasa-explore/src/components/home/Home.tsx
+++ nasa-explore/src/components/home/Home.tsx
@@ -1,12 +1,16 @@
 import {Component} from "react";
 import './Home.css';
+import headerLogo from './headerLogo.svg';
 
 export default class Home extends Component{
 
     render(){
         return(
             <div className="Homepage">
-                <p>dfgsdgsdgdkgdkslfmdkgndk</p>
+                <header className="Homepage-header">
+                    <img src={headerLogo} className="header-logo" alt="logo" />
+                    <a href="https://github.com/RenatoSerk">Designed by Renato. S</a>
+                </header>
             </div>
         );
     }
