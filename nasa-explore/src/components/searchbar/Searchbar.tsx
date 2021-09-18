b74952233428523a6fbd73e280ceaad5b3f3e922
Renato S <brickworked@gmail.com>
2021-09-18 14:25:21 -0400

    Add onsubmit functionality to searchbar
    Searchbar now has a prop function that is can be passed to it that will execute with the input data as a parameter when enter is pressed

--- nasa-explore/src/components/searchbar/Searchbar.tsx
+++ nasa-explore/src/components/searchbar/Searchbar.tsx
@@ -2,17 +2,29 @@ import React, { Component } from "react";
 import searchIcon from './search-icon.svg';
 import "./Searchbar.css"
 
-export default class Searchbar extends Component{
+export default class Searchbar extends Component<{onSubmit: (q: string) => void}>{
     state = {
-        query : '',
+        inputData: "",
         results : [],
         searchbarStyling : {borderRadius: '100px'},
         loading : false,
-        selectedOption : {id : '', posterURL : ''},
+        selectedOption : {id : '', posterURL : ''}
+    }
+
+    handleKeyDown(event: React.KeyboardEvent<HTMLDivElement>): void{
+        if (event.key === 'Enter'){
+            event.preventDefault();
+            event.stopPropagation();
+            this.props.onSubmit(this.state.inputData);
+        }
     }
 
     changeSearchbarStyling(radius : string): void{
         this.setState({searchbarStyling: {borderRadius: radius}});
+    }
+
+    handleOnInputChange(event: React.ChangeEvent<HTMLInputElement>): void{
+        this.setState({inputData : event.target.value});
     }
 
     render(){
@@ -20,9 +32,12 @@ export default class Searchbar extends Component{
             <div className="inputContainer">
                 <input className="Searchbar" id="searchbar"
                     placeholder="Search the stars for images!"
+                    style={this.state.searchbarStyling}
+                    value={this.state.inputData}
                     onSelect={() => this.changeSearchbarStyling('15px')}
                     onBlur={() => this.changeSearchbarStyling('100px')}
-                    style={this.state.searchbarStyling}/>
+                    onKeyDown={this.handleKeyDown.bind(this)}
+                    onChange={this.handleOnInputChange.bind(this)}/>
                 <img src={searchIcon} className="searchIcon" alt=""></img>
             </div>
         );
