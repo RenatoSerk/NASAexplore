55424152b2fbd33060482578ebaf71b53a088205
Renato S <brickworked@gmail.com>
2021-09-18 20:19:01 -0400

    Change App.tsx to router
    Changed App.tsx to be a router and moved functionality purely to components

--- nasa-explore/src/App.tsx
+++ nasa-explore/src/App.tsx
@@ -1,40 +1,19 @@
 import './App.css';
-import React, { useState } from 'react';
 import Splash from './components/splash/Splash';
 import Home from './components/home/Home';
-import FadeIn from 'react-fade-in';
+import { Route, Switch } from 'react-router';
+import Gallery from './components/gallery/Gallery';
+import Error from './components/error/Error';
 
 export default function App() {
-  const [count, setCount] = useState(1);
-  const [splashVisibility, setSplash] = useState(true);
-  const [splashFadedOut, setSplashFaded] = useState(false);
-  const [homeVisibility, setHome] = useState(false);
-
-  function fadeSplash(){
-    setCount(count + 1);
-    if (count === 2){
-      // Splash finished fading out, de-render it and render home
-      setSplashFaded(true);
-      setHome(true);
-    }
-    else{
-      // Fade in finished, start fade out
-      setSplash(false);
-    }
-  }
-
-  return (
-    <div className="App">
-      { splashFadedOut === false && 
-        <FadeIn transitionDuration={4000} onComplete={fadeSplash} visible={splashVisibility}>
-          <Splash/>
-        </FadeIn>
-      }
-      { homeVisibility === true &&
-        <FadeIn transitionDuration={2000}>
-          <Home/>
-        </FadeIn>
-      }
-    </div>
-  );
+ return(
+  <main>
+    <Switch>
+        <Route path="/" component={Splash} exact />
+        <Route path="/home" component={Home} />
+        <Route path="/gallery" component={Gallery} />
+        <Route component={Error} />
+    </Switch>
+</main>
+ )
 }
