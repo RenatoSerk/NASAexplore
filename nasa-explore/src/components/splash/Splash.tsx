5048a79392b773f4a81e24566591e701f9fde5ba
Renato S <brickworked@gmail.com>
2021-09-18 20:19:52 -0400

    Make splash appear for '/'
    The splash screen now only appears at '/' and will redirect to '/home' on finish

--- nasa-explore/src/components/splash/Splash.tsx
+++ nasa-explore/src/components/splash/Splash.tsx
@@ -3,15 +3,43 @@ import logo from './logo.svg';
 import nasa from './nasa.svg';
 import explore from './explore.svg';
 import './Splash.css'
+import FadeIn from "react-fade-in";
+import { Redirect } from "react-router";
 
 export default class Splash extends Component{
+    state = {
+        count: 0,
+        fadeSplash: false,
+        splashFaded: false,
+        splashVisibility: true
+    }
+
+    fadeSplash(){
+        this.setState({count: (this.state.count + 1)})
+        if (this.state.count === 2){
+          // Splash finished fading out, navigate to homepage
+          this.setState({splashFaded: true});
+        }
+        else{
+          // Fade in finished, start fade out
+          this.setState({splashVisibility: false});
+        }
+    }
 
     render(){
+        if (this.state.splashFaded === true){
+            return(<Redirect to="/home" />);
+        }
+
         return(
-            <div className="Splash">
-                <img src={nasa} alt="nasa" />
-                <img src={logo} className="App-logo" alt="logo" />
-                <img src={explore} alt="nasa" />
+            <div className="Splash-container">
+                <FadeIn transitionDuration={4000} onComplete={this.fadeSplash.bind(this)} visible={this.state.splashVisibility}>
+                    <div className="Splash">
+                        <img src={nasa} alt="nasa" />
+                        <img src={logo} className="App-logo" alt="logo" />
+                        <img src={explore} alt="nasa" />
+                    </div>
+                </FadeIn>
             </div>
         );
     }
