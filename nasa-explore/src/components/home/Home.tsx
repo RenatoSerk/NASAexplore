d2b2056fe666f7dd091e54ac9777d03df846921f
Renato S <brickworked@gmail.com>
2021-09-18 20:22:35 -0400

    Add redirect to '/gallery'
    Home component will now redirect to '/gallery' when the searchbar component returns a query.

--- nasa-explore/src/components/home/Home.tsx
+++ nasa-explore/src/components/home/Home.tsx
@@ -1,19 +1,35 @@
 import {Component} from "react";
+import {Redirect} from 'react-router-dom';
 import './Home.css';
-import headerLogo from './headerLogo.svg';
 import Searchbar from "../searchbar/Searchbar";
+import Header from "../header/Header";
+import FadeIn from "react-fade-in";
 
 export default class Home extends Component{
+    state = {
+        searchQuery: "",
+    }
 
     render(){
+        if (this.state.searchQuery !== ""){
+            let urlQuery = '/gallery?q=' + this.state.searchQuery;
+            return (
+                <Redirect to={urlQuery} />
+            )
+        }
+
         return(
-            <div className="Homepage">
-                <header className="Homepage-header">
-                    <img src={headerLogo} className="header-logo" alt="logo" />
-                    <a href="https://github.com/RenatoSerk">Designed by Renato. S</a>
-                </header>
-                <Searchbar/>
-            </div>
+            <main className="Home-container">
+                <FadeIn transitionDuration={2000}>
+                    <div className="Homepage">
+                        <Header/>
+                        <Searchbar onSubmit={(q: string) => {this.setState({searchQuery: q})}}/>
+                        { this.state.searchQuery !== "" &&
+                        <p>{this.state.searchQuery}</p>
+                        }
+                    </div>
+                </FadeIn>
+            </main>
         );
     }
 }
