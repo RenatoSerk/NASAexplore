7ea6d781c74a12453a3d3d357018b077613d8c65
Renato S <brickworked@gmail.com>
2021-09-19 19:58:55 -0400

    Add API response interface

---
+++ nasa-explore/src/models/ApiResults.ts
@@ -0,0 +1,24 @@
+export interface ApiResults{
+    collection: {
+        items: [ImageItem],
+        metadata: {
+            total_hits: number
+        }
+    }
+}
+
+export interface ImageItem{
+    data: [
+        {
+            center: string,
+            date_created: string,
+            description: string,
+            title: string
+        }
+    ]
+    links: [
+        {
+            href: string
+        }
+    ]
+}
