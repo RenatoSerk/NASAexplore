b322fcc04874f1c0eb3f7a290916c7c6a86a8d01
Renato S <brickworked@gmail.com>
2021-09-20 00:18:14 -0400

    Add modal to imagecard
    Image cards will now pop up with a modal card when clicked, and maintain their scroll position while disabling scrolling.

--- nasa-explore/src/components/gallery/image-card/ImageCard.tsx
+++ nasa-explore/src/components/gallery/image-card/ImageCard.tsx
@@ -1,15 +1,51 @@
 import { Component } from "react";
 import { ImageItem } from "../../../models/ApiResults";
+import CardModal from "./cardModal/CardModal";
 import './ImageCard.css'
 
 export default class ImageCard extends Component<{image: ImageItem}>{
-    state = {}
+    state = {
+        showModal: false
+    }
+
+    handleOnClick(){
+        this.toggleModal();
+    }
+
+    toggleModal(){
+        // Disable scroll if modal is showing and maintain scroll position
+        if (!this.state.showModal){
+            let rootElement: HTMLElement|null = document.getElementById('root');
+            if (rootElement !== null){
+                rootElement.style.top = `-${window.scrollY}px`;
+                rootElement.style.position = 'fixed';
+            }
+        }
+        // Enable scroll if not showing modal, and scroll back to
+        // where we were before the modal showed!
+        else{
+            let rootElement: HTMLElement|null = document.getElementById('root');
+            if (rootElement !== null){
+                const scrollY = rootElement.style.top;
+                rootElement.style.top = ``;
+                rootElement.style.position = '';
+                window.scrollTo(0, parseInt(scrollY || '0') * -1);
+            }
+        }
+        this.setState({showModal: !this.state.showModal});
+    }
 
     render(){
         return(
             <div className="Card">
-                <img src={this.props.image.links[0].href} alt="NASA IMG" className="Image"/>
+                <img src={this.props.image.links[0].href} 
+                    alt={this.props.image.data[0].title} 
+                    className="Image"
+                    onClick={this.handleOnClick.bind(this)}/>
+                    <CardModal image={this.props.image} 
+                        show={this.state.showModal}
+                        onclose={this.toggleModal.bind(this)}/>
             </div>
-        )
+        );
     }
 }
